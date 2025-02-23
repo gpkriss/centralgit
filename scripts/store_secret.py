652e37a2bf2992460d7351e07679c61f6f7012e3
gpkriss <117080585+gpkriss@users.noreply.github.com>
2025-02-23 22:06:26 +0530

    Update store_secret.py

--- scripts/store_secret.py
+++ scripts/store_secret.py
@@ -11,10 +11,10 @@ with open('secrets.json', 'r') as file:
 # Store each secret individually
 for secret_name, secret_value in secrets.items():
     try:
-        # Check if the secret exists
         client.get_secret_value(SecretId=secret_name)
         client.update_secret(SecretId=secret_name, SecretString=secret_value)
         print(f"Updated existing secret: {secret_name}")
     except client.exceptions.ResourceNotFoundException:
         client.create_secret(Name=secret_name, SecretString=secret_value)
         print(f"Created new secret: {secret_name}")
+)
